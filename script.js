b9466b3aa1ad0a37d1b737251b0ed204cde0050e
Haruhi Suzumiya <thesosbrigade@yahoo.com>
2025-01-25 21:24:38 -0500

    confusing

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(function() {
-    $("button").on({
-      mouseover: function() {
-        $(this).css({
-          left: (Math.random() * 200) + "px",
-          top: (Math.random() * 200) + "px",
-        });
+$(function(){
+  $("button").on({
+      mouseover:function(){
+          $(this).css({
+              left:(Math.random()*200)+"px",
+              top:(Math.random()*200)+"px",
+          });
       }
-    });
   });
+});
